10760cd2d31d2f43d840f4e92ef15099319c938d
Irkham Widhi Saputro <fishblacklance@gmail.com>
2018-12-18 20:24:31 +0700

    add register form

--- NBC/views/admin/auth/view.py
+++ NBC/views/admin/auth/view.py
@@ -1,8 +1,38 @@
+import datetime
 from flask import render_template, redirect, flash, url_for, request
 from flask_login import login_user, login_required, logout_user
 
 from . import auth
 from NBC.models.user import User
+from NBC.service.database_service import save_to_db
+
+
+@auth.route('/admin/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        first_name = request.form.get('firstname')
+        last_name = request.form.get('lastname')
+        phone = request.form.get('phone')
+        if not email or not password or not first_name or not last_name:
+            flash('Please fill all form!', 'danger')
+            return redirect(url_for('dashboard.create_user'))
+        if phone:
+            phone = '+62' + phone
+        data = User(
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            registered_on=datetime.datetime.utcnow(),
+            phone_number=phone,
+            role='Admin'
+        )
+        save_to_db(data)
+        flash('User berhasil dibuat', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('register.html')
 
 
 @auth.route('/admin/login', methods=["GET", "POST"])
